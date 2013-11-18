7b160b0650b1c7cd3f6188134af7c089cf820d78
Thomas Browder <tom.browder@gmail.com>
2013-11-18 19:44:12 +0000

    add missing lines

--- VM-user-home-files/brlcad-config.sh
+++ VM-user-home-files/brlcad-config.sh
@@ -3,5 +3,7 @@
 cmake ../brlcad-svn-trunk         \
                                   \
      -DBRLCAD_BUNDLED_LIBS=ON     \
+     -DBRLCAD_SCL=ON              \
      -DBRLCAD_ZLIB=OFF            \
+     -DBRLCAD_ENABLE_OPENGL=ON    \
      -DCMAKE_BUILD_TYPE=DEBUG
